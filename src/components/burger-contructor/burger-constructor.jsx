a068b1c00e140f69674bda661d071997d7dd8ab0
Ivan Storozhenko <ivan.storojenko@gmail.com>
2025-05-11 19:32:14 +0300

    feat(burgerconstructor): реализовано отображение конструктора бургера (данные пока захордкожены)

--- src/components/burger-contructor/burger-constructor.jsx
+++ src/components/burger-contructor/burger-constructor.jsx
@@ -2,9 +2,63 @@ import React from 'react';
 import styles from './burger-constructor.module.css';
 import * as PropTypes from 'prop-types';
 import { ingredientPropType } from '@utils/prop-types.js';
+import {
+	ConstructorElement,
+	DragIcon,
+	CurrencyIcon,
+	Button,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
-export const BurgerConstructor = () => {
-	return <section className={styles.burger_constructor}></section>;
+export const BurgerConstructor = ({ ingredients }) => {
+	const bun = ingredients.filter((ingredient) => ingredient.type === 'bun')[0];
+	const restIngredients = ingredients.filter(
+		(ingredient) => ingredient.type !== 'bun'
+	);
+
+	return (
+		<section className={`${styles.burger_constructor}  mt-25`}>
+			<ConstructorElement
+				key='top'
+				text={bun.name}
+				thumbnail={bun.image}
+				price={bun.price}
+				type='top'
+				isLocked={true}
+				extraClass='ml-8'
+			/>
+			<ul className={`${styles.filling_list} custom-scroll`}>
+				{restIngredients.map((ingredient) => (
+					<li className={`${styles.filling_item}`}>
+						<DragIcon type='primary' />
+						<ConstructorElement
+							key={ingredient._id}
+							text={ingredient.name}
+							thumbnail={ingredient.image}
+							price={ingredient.price}
+						/>
+					</li>
+				))}
+			</ul>
+			<ConstructorElement
+				key='bottom'
+				text={bun.name}
+				thumbnail={bun.image}
+				price={bun.price}
+				type='bottom'
+				isLocked={true}
+				extraClass='ml-8'
+			/>
+			<div className={`${styles.complete_order} mt-10`}>
+				<div>
+					<span className='text text_type_digits-medium mr-2'>610</span>
+					<CurrencyIcon type='primary' />
+				</div>
+				<Button htmlType='button' type='primary' size='medium'>
+					Оформить заказ
+				</Button>
+			</div>
+		</section>
+	);
 };
 
 BurgerConstructor.propTypes = {
