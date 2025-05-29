fc0a661c0f70f7716603e355d0a4cf4a137811c4
Иван Ильич Стороженко <I_Storozhenko@volganet.ru>
2025-05-29 15:53:11 +0300

    feat(ingredient-item): counter отображается в зависимости от состояния конструктора

--- src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
+++ src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
@@ -3,14 +3,25 @@ import {
 	Counter,
 	CurrencyIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentIngredient } from '@services/ingredient-detail/actions';
 import { useDrag } from 'react-dnd';
 import { ingredientPropType } from '@utils/prop-types';
+import { getConstructorIngredients } from '@services/burger-constructor/reducer';
 
 export const IngredientItem = ({ ingredient }) => {
-	const { _id, name, price, image } = ingredient;
+	const { _id, type, name, price, image } = ingredient;
 	const dispatch = useDispatch();
+	const constructorIngredients = useSelector(getConstructorIngredients);
+
+	let counter = 0;
+	if (type === 'bun') {
+		counter = constructorIngredients.bun?._id === _id ? 2 : 0;
+	} else {
+		counter = constructorIngredients.ingredients.filter(
+			(item) => item._id === _id
+		).length;
+	}
 
 	const handleSetCurrentIngredient = () => {
 		dispatch(setCurrentIngredient(_id));
@@ -26,7 +37,9 @@ export const IngredientItem = ({ ingredient }) => {
 			<button
 				className={styles.ingredient_item}
 				onClick={handleSetCurrentIngredient}>
-				<Counter count={1} size='default' extraClass='m-1' />
+				{counter !== 0 && (
+					<Counter count={counter} size='default' extraClass='m-1' />
+				)}
 				<img src={image} alt={name} className={`${styles.image}`} />
 				<div className={styles.price}>
 					<span className='text text_type_digits-default mr-2'>{price}</span>
