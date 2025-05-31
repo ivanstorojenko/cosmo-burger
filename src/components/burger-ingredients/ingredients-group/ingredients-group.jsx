9970281dcd85c356010c6ee3d3185ebc147f7ff3
Ivan Storozhenko <ivan.storojenko@gmail.com>
2025-05-31 23:14:45 +0300

    refactor(ingredient-group): названия типов ингредиентов теперь получаются из хранилища

--- src/components/burger-ingredients/ingredients-group/ingredients-group.jsx
+++ src/components/burger-ingredients/ingredients-group/ingredients-group.jsx
@@ -2,21 +2,22 @@ import { useSelector } from 'react-redux';
 import { IngredientItem } from '../ingredient-item/ingredient-item';
 import styles from './ingredients-group.module.css';
 import * as PropTypes from 'prop-types';
-import { getAllIngredients } from '@services/burger-ingredients/reducer';
+import {
+	getAllIngredients,
+	getIngredientTypes,
+} from '@services/burger-ingredients/reducer';
 
 export const IngredientsGroup = ({ type }) => {
 	const ingredients = useSelector(getAllIngredients);
+	const ingredientTypes = useSelector(getIngredientTypes);
+	const typeName = ingredientTypes.find((item) => item.type === type).name;
 	const ingredientsInType = ingredients.filter(
 		(ingredient) => ingredient.type === type
 	);
 
 	return (
 		<li className={styles.ingredient_group}>
-			<h2 className='text text_type_main-medium'>
-				{type === 'bun' && 'Булки'}
-				{type === 'main' && 'Начинки'}
-				{type === 'sauce' && 'Соусы'}
-			</h2>
+			<h2 className='text text_type_main-medium'>{typeName}</h2>
 			<ul className={`${styles.ingredient_list} pr-4 pl-4`}>
 				{ingredientsInType.map((ingredient) => (
 					<IngredientItem key={ingredient._id} ingredient={ingredient} />
